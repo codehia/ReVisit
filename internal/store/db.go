f8ec55562ea381faf118fe8e0ece9b23c5ee905b
codehia <dev@sacharya.dev>
2026-05-10 04:17:48 +0530

    Refactor db.go for new db connection creation

--- internal/store/db.go
+++ internal/store/db.go
@@ -2,9 +2,16 @@ package store
 
 import (
 	"database/sql"
+	"os"
 
 	"github.com/pressly/goose/v3"
+	"go.uber.org/zap"
 	_ "modernc.org/sqlite"
+)
+
+var (
+	logger = zap.Must(zap.NewDevelopment())
+	sugar  = logger.Sugar()
 )
 
 type Store struct {
@@ -17,7 +24,7 @@ func New(path string) (*Store, error) {
 		return nil, err
 	}
 
-	goose.SetDialect("sqlite3")
+	goose.SetDialect("sqlite3") //nolint:errcheck
 	if err := goose.Up(db, "migrations"); err != nil {
 		return nil, err
 	}
@@ -26,3 +33,17 @@ func New(path string) (*Store, error) {
 }
 
 func (s *Store) DB() *sql.DB { return s.db }
+
+func Open() (*sql.DB, error) {
+	databaseName := os.Getenv("DATABASE_NAME")
+	if databaseName == "" {
+		sugar.Warnw("DATABASE_NAME is not set. Setting it to goflash.db")
+		databaseName = "goflash.db"
+	}
+	s, err := New(databaseName)
+	if err != nil {
+		sugar.Fatalf("failed to open store: %v", err)
+		return nil, err
+	}
+	return s.DB(), nil
+}
